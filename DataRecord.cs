0fb872fb5973a1abdf001efcace2fb73adb60dac
Halime Nur YALÇIN <nurrevrenn.01@gmail.com>
2025-01-12 23:21:32 +0300

    Add DataRecord.cs to data model class

---
+++ DataRecord.cs
@@ -0,0 +1,15 @@
+﻿namespace MartenProject;
+
+public class DataRecord
+{
+    public string Key { get; set; }
+    public Person Data { get; set; }
+}
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Gender { get; set; }
+    public string Occupation { get; set; }
+    public string MaritalStatus { get; set; }
+}
